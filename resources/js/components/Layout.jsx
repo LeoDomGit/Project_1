187a371096cc938da6c6970a394031db56d85981
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-10-02 16:27:22 +0700

    xCommit

--- resources/js/components/Layout.jsx
+++ resources/js/components/Layout.jsx
@@ -5,6 +5,8 @@ import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import GroupIcon from '@mui/icons-material/Group';
 import "@chatscope/chat-ui-kit-styles/themes/default/main.scss";
 import ChatIcon from '@mui/icons-material/Chat';
+import CategoryIcon from '@mui/icons-material/Category';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
 import "../../css/app.css";
 function Layout({ children }) {
   const { collapseSidebar } = useProSidebar();
@@ -30,6 +32,15 @@ function Layout({ children }) {
             <a href={'/admin/permissions'}> <MenuItem icon={<GroupIcon />}>Quyền tài khoản</MenuItem></a>
            <a href={'/admin/roles'}> <MenuItem icon={<GroupIcon />}>Loại tài khoản</MenuItem></a>
            <a href={'/admin/users'}> <MenuItem icon={<GroupIcon />}>Tài khoản</MenuItem></a>
+            </SubMenu>
+
+            <SubMenu label="Products" className='rounded border-2' icon={<CategoryIcon />}>
+            <a href={'/admin/brands'}> <MenuItem icon={<CategoryIcon />}>Thương hiệu</MenuItem></a>
+           <a href={'/admin/categories'}> <MenuItem icon={<CategoryIcon />}>Loại sản phẩm</MenuItem></a>
+           <SubMenu label="Sản phẩm" className='rounded border-2' icon={<Inventory2Icon />}>
+            <a href={'/admin/products/create'}> <MenuItem icon={<CategoryIcon />}>Tạo sản phẩm</MenuItem></a>
+           <a href={'/admin/products'}> <MenuItem icon={<Inventory2Icon />}>Sản phẩm</MenuItem></a>
+            </SubMenu>
             </SubMenu>
             <Menu>
               <a href="/chat"><MenuItem icon={<ChatIcon />}>Chat</MenuItem></a>

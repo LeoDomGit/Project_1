11b234b13a4400e90ecc93f3b17953e09b761beb
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-10-07 10:05:12 +0700

    Update SignIn.jsx

--- resources/js/Pages/Login/SignIn.jsx
+++ resources/js/Pages/Login/SignIn.jsx
@@ -95,7 +95,7 @@ export default function SignInSide() {
             }).catch((error) => {
                 notyf.open({
                     type: "error",
-                    message: "An error occurred. Please try again.",
+                    message: "Sai tên đăng nhập hoặc mật khẩu.",
                 });
             });
         }
